1390c1dc86d88c201addce653217f1aefa868644
Fatema <fatemae110@yahoo.com>
2017-02-14 23:32:59 +0530

    Custom Filters

--- expressionAndInterpolation/app.js
+++ expressionAndInterpolation/app.js
@@ -2,22 +2,46 @@
 'use strict';
 
 angular.module("feedApp",[])
-.controller("MsgController",MsgController);
+.controller("MsgController",MsgController)
+.filter('loves',lovesFilter)
+.filter('truth',truthFilter);
 
-MsgController.$inject=['$scope'];
-function MsgController($scope){
+MsgController.$inject=['$scope','lovesFilter','$filter'];
+function MsgController($scope,lovesFilter,$filter){
   $scope.name="FATEMA";
   $scope.stateOfBeing="hungry";
+  $scope.foodCost=.45;
 
   $scope.feedFatema=function(){
     $scope.stateOfBeing="full";
   }
 
   $scope.sayMessage=function(){
-    return "Fatema likes to eat healthy food.";
+    var msg="Fatema likes to eat healthy food.";
+    var output=$filter('uppercase')(msg);
+    return output;
   }
 
+  $scope.sayLovesMessage=function(){
+    var msg="Fatema likes to eat healthy food.";
+    msg=lovesFilter(msg);
+    var output=$filter('uppercase')(msg);
+    return output;
+  }
 }
 
+function lovesFilter(){
+  return function(input){
+    input=input.replace("likes","loves");
+    return input;
+  };
+}
+
+function truthFilter(){
+  return function(input,target,replace){
+    input=input.replace(target,replace);
+    return input;
+  };
+}
 
 })();
