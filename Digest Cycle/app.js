9fb643b29d71c714faa3ea0ce71db99913036dcc
Fatema <fatemae110@yahoo.com>
2017-02-15 17:25:44 +0530

    Digest Cycle

--- Digest Cycle/app.js
+++ Digest Cycle/app.js
@@ -6,11 +6,37 @@ angular.module('CounterApp',[])
 
 CounterCtrl.$inject=['$scope'];
 function CounterCtrl($scope){
+  $scope.onceCounter=0;
+  $scope.upCounter=0;
+  $scope.name="Fatema";
 
   $scope.showNumberOfWatchers=function(){
+    //console.log($scope);
     console.log('# of watchers:', $scope.$$watchersCount);
 
   };
+
+  $scope.countOnce=function(){
+    $scope.onceCounter=1;
+  };
+
+  $scope.incrementCounter=function(){
+    $scope.upCounter++;
+  };
+
+  $scope.$watch(function(){
+    console.log("Digest Loop Fired!!");
+  });
+
+  // $scope.$watch('onceCounter', function (newValue,oldValue){
+  //   console.log("onceCounter old Value: ",oldValue);
+  //   console.log("onceCounter new Value: ",newValue);
+  // });
+  //
+  // $scope.$watch('upCounter', function (newValue,oldValue){
+  //   console.log("upCounter old Value: ",oldValue);
+  //   console.log("upCounter new Value: ",newValue);
+  // });
 
 }
 })();
